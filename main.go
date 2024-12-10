75641734aab7046e6a3dea3d22681aa54bcccdf5
ChatBear <adamaly.shiraz@outlook.fr>
2024-12-11 01:07:43 +0700

    stream log is working

--- main.go
+++ main.go
@@ -16,11 +16,25 @@ func check(err error) {
 	if err != nil {
 		panic(err)
 	}
-
 }
+
+func formatDate(timestamps int64) string {
+	seconds := timestamps / 1000
+	dateTime := time.Unix(seconds, 0)
+	formattedDate := dateTime.Format("2006-01-02 15:04:05")
+	return formattedDate
+}
+
+// func main() {
+// 	timestamp := retrieveLogCloudwatch("apiTemplate", 0)
+// 	for {
+// 		timestamp = retrieveLogCloudwatch("apiTemplate", timestamp) + 1
+// 	}
+// }
+
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: my-cli-app <command> [options]")
+		fmt.Println("Usage: stream <command> [options]")
 		os.Exit(1)
 	}
 
@@ -30,7 +44,8 @@ func main() {
 
 	case "logs":
 		stopChan := make(chan struct{})
-
+		lambdaName := os.Args[2]
+		timestamp := retrieveLogCloudwatch(lambdaName, 0) + 1
 		go func() {
 			reader := bufio.NewReader(os.Stdin)
 			fmt.Println("Press enter to stop")
@@ -43,7 +58,7 @@ func main() {
 				fmt.Println("Loop stopped by user.")
 				return
 			default:
-				retrieveLogCloudwatch("apiTemplate")
+				timestamp = retrieveLogCloudwatch(lambdaName, timestamp)
 				time.Sleep(1 * time.Second)
 			}
 		}
@@ -64,51 +79,51 @@ func retrieveCreds() {
 	fmt.Printf("Here's the content of the file : %s", dat)
 }
 
-func retrieveLogCloudwatch(name string) {
+func retrieveLogCloudwatch(name string, timestamp int64) int64 {
 	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("ap-southeast-1"))
 	check(err)
 
 	client := cloudwatchlogs.NewFromConfig(cfg)
 	logGroupName := fmt.Sprintf("/aws/lambda/%s", name)
 
-	// currentTime := time.Now().Unix() * 1000 // Convert to milliseconds
-	// startTime := currentTime - 1000
-	// input := &cloudwatchlogs.GetLogEventsInput{
-	// 	LogGroupName: aws.String(logGroupName),
-	// 	// Limit:        aws.Int32(10),
-	// 	// StartTime: aws.Int64(startTime),
-	// 	// EndTime:      aws.Int64(currentTime),
-
-	// }
-
-	// fmt.Println(input)
 	inputStream := &cloudwatchlogs.DescribeLogStreamsInput{
 		LogGroupName: aws.String(logGroupName),
+		Limit:        aws.Int32(1),
 		Descending:   aws.Bool(true),
+		OrderBy:      "LastEventTime",
 	}
 
-	// resp, err := client.GetLogEvents(context.TODO(), input)
 	logStream, err := client.DescribeLogStreams(context.TODO(), inputStream)
 	check(err)
 
-	// if len(resp.Events) == 0 {
-	// 	fmt.Printf("\nNo Events\n")
-	// 	return
-	// }
-	fmt.Println(logStream.LogStreams)
-
-	for _, stream := range logStream.LogStreams {
-		timestamp := time.Unix(*stream.CreationTime/1000, 0)
-		fmt.Println(timestamp.Format("2006-01-02 15:04:05"))
-
+	startTime := timestamp
+	input := &cloudwatchlogs.GetLogEventsInput{
+		LogGroupName:  aws.String(logGroupName),
+		LogStreamName: aws.String(*logStream.LogStreams[0].LogStreamName),
+		StartTime:     aws.Int64(startTime),
+		// EndTime:   aws.Int64(currentTime),
 	}
-	// for _, event := range resp.Events {
-	// 	// timestamp := time.Unix(*event.Timestamp/1000, 0)
-	// 	// // Format the timestamp to a classic date format
-	// 	// fmt.Println(timestamp.Format("2006-01-02 15:04:05"))
-	// 	fmt.Printf(*event.Message)
-	// }
-	// for event_index_desc_order := len(resp.Events) - 1; event_index_desc_order >= 0; event_index_desc_order-- {
-	// 	fmt.Printf(*resp.Events[event_index_desc_order].Message)
+	resp, err := client.GetLogEvents(context.TODO(), input)
+	// fmt.Println(startTime)
+	// seconds := startTime / 1000
+	// dateTime := time.Unix(seconds, 0)
+	// formattedDate := dateTime.Format("2006-01-02 15:04:05")
+	// fmt.Println(formattedDate)
+	// fmt.Println(*logStream.LogStreams[0].LogStreamName)
+	// fmt.Println(len(resp.Events))
+	for _, event := range resp.Events {
+		if *event.Timestamp > timestamp {
+			timestamp = int64(*event.Timestamp)
+		}
+
+		fmt.Printf(*event.Message)
+	}
+	check(err)
+
+	return timestamp
+	// for _, stream := range logStream.LogStreams {
+	// 	timestamp := time.Unix(*stream.CreationTime/1000, 0)
+	// 	fmt.Println(timestamp.Format("2006-01-02 15:04:05"))
+	// 	fmt.Println(*stream.LogStreamName)
 	// }
 }
